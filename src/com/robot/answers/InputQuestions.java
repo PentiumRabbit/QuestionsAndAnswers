1bb3ed1fbc827abac061737e6d2fcf82a499f949
zhaoruyang <zhaoruyang@baofeng.com>
2016-06-06 11:26:06 +0800

    init

---
+++ src/com/robot/answers/InputQuestions.java
@@ -0,0 +1,13 @@
+package com.robot.answers;
+
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class InputQuestions extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        ActionManager actionManager = e.getActionManager();
+    }
+}
